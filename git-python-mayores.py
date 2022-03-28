280c14003b26bbebd41bd77faaa245ad5fd8f201
MayoresMycaMae <mayoresmyca@gmail.com>
2022-03-28 14:45:32 +0800

    My Educational Background added

--- git-python-mayores.py
+++ git-python-mayores.py
@@ -1,4 +1,8 @@
 print ("Myca Mae L. Mayores")
 print ("Ino Mogpog Marinduque")
 print ("April 21, 2002")
+print ("Marinduque State College")
+print ("Bachelor of Information Technology")
+print ("2nd Year College")
+print ("2F")
 
